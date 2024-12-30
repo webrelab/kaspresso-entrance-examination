1a95b293edbdb0041609f74b908ce62aad25cb28
Mxx <mlyn@tut.by>
2024-12-30 16:54:55 +0300

    Some optimizations

--- src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
+++ src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
@@ -23,30 +23,28 @@ class CerealStorageImpl(
     override fun addCereal(cereal: Cereal, amount: Float): Float {
         if (amount < 0) throw IllegalArgumentException("Cannot add negative amount of cereal")
         val cerealAmount = (storage[cereal] ?: 0f) + amount
-        if ((cereal !in storage.keys)
-            && (storageCapacity - storage.keys.count() * containerCapacity < containerCapacity)
+        if ((cereal !in storage)
+            && (storageCapacity - storage.size * containerCapacity < containerCapacity)
         ) throw IllegalStateException("Cannot add another container")
-        storage[cereal] = if (cerealAmount < containerCapacity) cerealAmount else containerCapacity
-        var remainder = cerealAmount - containerCapacity
-        remainder = if (remainder >= 0) remainder else 0f
-        return remainder
+        storage[cereal] = minOf(cerealAmount, containerCapacity)
+        return maxOf(cerealAmount - containerCapacity, 0f)
     }
 
     override fun getCereal(cereal: Cereal, amount: Float): Float {
         if (amount < 0) throw IllegalArgumentException("Cannot get negative amount")
         val amountObtained = storage[cereal]?.let {
-            if (it >= amount) amount else storage[cereal]
+            if (it >= amount) amount else it
         } ?: throw IllegalStateException("There is no such cereal in the storage")
         storage[cereal] = (storage[cereal] ?: 0f) - amountObtained
         return amountObtained
     }
 
     override fun removeContainer(cereal: Cereal): Boolean {
-        if (storage[cereal] == null) throw IllegalStateException("No such container")
-        if (storage[cereal] == 0f) {
+        val amount = storage[cereal] ?: throw IllegalStateException("No such container")
+        if (amount == 0f) {
             val result = storage.remove(cereal)?.let {
                 it == 0f
-            } ?: throw IllegalStateException("No such container")
+            } ?: throw IllegalStateException("Failed to remove container")
             return result
         }
         return false
